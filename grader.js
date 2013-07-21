d186288f12da6b5877534bc4c9f9ceb86600cf00
Roofi Shaikh <roofishaikh@gmail.com>
2013-07-21 00:39:17 +0000

    Final checkeck 1

--- grader.js
+++ grader.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-   /*
-Automatically grade files for the presence of specified HTML tags/attributes.
+/*
+Automatically grade files for the presence of specified HTML tags/attributes
 Uses commander.js and cheerio. Teaches command line application development
 and basic DOM parsing.
 
@@ -19,41 +19,49 @@ References:
    - http://en.wikipedia.org/wiki/JSON
    - https://developer.mozilla.org/en-US/docs/JSON
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
-   */
+*/
 
-    var fs = require('fs');
+var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	console.log("%s does not exist. Exiting.", instr);
+	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var htmlFileContent = function(htmlfile) {
+    return fs.readFileSync(htmlfile);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+var checkJSONTags = function(htmlContent,checksfile){
+   $ = cheerio.load(htmlContent);
+   var checks = loadChecks(program.checks).sort();
+   var out = {};
+   for(var ii in checks){
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
     }
-    return out;
+    var outJson = JSON.stringify(out,null,4);
+    return outJson;
 };
+
+/* Check for HTML Tags using JSON */
+var checkHtmlFile = function(htmlfile, checksfile) {
+    return checkJSONTags(htmlFileContent(htmlfile),checksfile);
+};
+
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -61,14 +69,36 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var restlerHandler = function(result,response){
+    if(result instanceof Error){
+	console.log("Error fetching URL" + programs.url);
+    }
+    //console.log(result);
+    var outJson = checkJSONTags(result,program.checks);
+    console.log(outJson);
+ };
+
+
+
+
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'url')
+	.parse(process.argv);
+
+    if(program.url){
+	//console.log("You entered url : " + program.url);
+	restler.get(program.url).on('complete',restlerHandler);
+
+    }
+    else {
+	var outJson = checkHtmlFile(program.file, program.checks);
+	console.log(outJson);
+
+    }
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
